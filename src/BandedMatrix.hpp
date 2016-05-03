6817a1cddae8dc83f10ceebac225c6778d4b1de8
Russley Shaw <russleyshaw@gmail.com>
2016-05-03 14:01:33 -0500

    Compiling BandedMatrix

--- src/BandedMatrix.hpp
+++ src/BandedMatrix.hpp
@@ -22,22 +22,42 @@ BandedMatrix<T>::BandedMatrix(BandedMatrix<ValueType>&& other)
 template<class T>
 BandedMatrix<T>& BandedMatrix<T>::operator=(const BandedMatrix<T>& other)
 {
+  assert(other.m_rows == other.m_cols);
+  assert(other.m_type == MatrixType::BANDED);
+  if(this != &other)
+  {
+    this->m_type = MatrixType::BANDED;
+    this->m_rows = other.m_rows;
+    this->m_cols = other.m_cols;
+    m_data = other.m_data;
+  }
   return *this;
 }
 
 template<class T>
 BandedMatrix<T>& BandedMatrix<T>::operator=(BandedMatrix<T>&& other)
 {
+  assert(other.m_rows == other.m_cols);
+  assert(other.m_type == MatrixType::BANDED);
+  if(this != &other)
+  {
+    this->m_type = MatrixType::BANDED;
+    this->m_rows = other.m_rows;
+    this->m_cols = other.m_cols;
+    m_data = std::move(other.m_data);
+  }
   return *this;
 }
 
 template<class T>
-T BandedMatrix<T>::get(const SizeType row, const SizeType col) const
+typename BandedMatrix<T>::ValueType BandedMatrix<T>::get(const SizeType row, const SizeType col) const
 {
-  return T
+  //TODO: BandedMatrix<T>::get logic
+  return ValueType(0);
 }
 
 template<class T>
 void BandedMatrix<T>::set(const SizeType row, const SizeType col, ConstReferenceType value)
 {
+  //TODO: BandedMatrix<T>::get logic
 }
